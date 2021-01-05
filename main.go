404527732fa3fce28659712401d6eaac18c43848
Jesse Millar <jesse.millar@microsoft.com>
2021-01-05 09:23:37 -0600

    Adding a version number

--- main.go
+++ main.go
@@ -3,14 +3,23 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 	"strings"
 
 	"github.com/nathan-fiscaletti/consolesize-go"
 )
 
+const version = "0.1.0"
+
 func main() {
+	versionFlag := flag.Bool("v", false, "prints current roxy version")
 	separatorCharacter := flag.String("d", "-", "The character used to draw the full-width separator")
 	flag.Parse()
+
+	if *versionFlag {
+		fmt.Println(version)
+		os.Exit(0)
+	}
 
 	cols, _ := consolesize.GetConsoleSize()
 
